4a6b98205c9edcf5ff871aeea0bf306ec9d94588
Merchant_Daniel <merchantdaniel8@gmail.com>
2023-12-13 13:33:26 +0000

    dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         // home: const MyHomePage(title: 'Flutter Demo Home Page'),
 
         initialRoute: FirebaseAuth.instance.currentUser == null
-            ? '/landing'
+            ? '/dashboard'
             : '/dashboard',
         routes: {
           // "/splash":(context) => SplashScreen(),
